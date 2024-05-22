a74a5bff9a9c3544ad2cc7122dbc72c70f90d21a
Richelle <arat.richellemae@gmail.com>
2024-05-23 01:49:03 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import LoginPage from "./Pages/Login";
+import Dashboard from "./Pages/Dashboard";
+import Signup from "./Pages/Signup";
+
+function App() {
+  return (
+    <>
+      <LoginPage />
+
+    </>
+  );
+}
+
+export default App;
